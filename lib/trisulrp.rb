439cbf62170bf913a280397a5f26e10577f06d03
vivek <vivek_rajagopal@yahoo.com>
2010-12-30 17:52:59 +0530

    Major changes

---
+++ lib/trisulrp.rb
@@ -0,0 +1,15 @@
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
+# =TRISUL TRP 
+# #
+# # 
+# # Author:: Vivek Rajagopalan
+# # Date:: 30 Dec 2010
+
+module TRP
+end
+
+require 'rubygems'
+require 'trplib.rb'
+require 'trp.pb'
