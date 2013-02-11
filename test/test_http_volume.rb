5036c89de0878d303f72155789e77509cbf7df51
vivek <vivek_rajagopal@yahoo.com>
2013-02-11 18:38:42 +0530

    Test volume script

--- test/test_http_volume.rb
+++ test/test_http_volume.rb
@@ -9,10 +9,12 @@ conn  = connect(ARGV[0],ARGV[1],"Demo_Client.crt","Demo_Client.key")
 tmarr = get_available_time(conn)
 
 # send request for http ( cg=APPS, key=p-0050) 
+# notice use of volumes_only => 1 
 req = TrisulRP::Protocol.mk_request(TRP::Message::Command::COUNTER_ITEM_REQUEST,
                                     :counter_group=> CG_APP,
                                     :key=> 'p-0050',
-                                    :time_interval => mk_time_interval(tmarr) )
+                                    :time_interval => mk_time_interval(tmarr),
+									:volumes_only => 1 )
 
 # print totals 
 get_response(conn,req) do |resp|
